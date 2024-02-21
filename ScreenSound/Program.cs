b36055adbc1374dd92d038ca033f189b8d765171
Pietra Carneiro <pietrasc98@gmail.com>
2024-02-20 23:13:57 -0300

    Criando classe Album, adicionando e exibindo as músicas do Album

--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,25 +1,85 @@
-﻿//Projeto com orientação a objetos em C# - Tema: Música
-
-// nome
-// artista
-// duração
-// disponível
+﻿Album albumDaBeyonce = new Album();
+albumDaBeyonce.Nome = "Renaissance";
 
 Musica musica1 = new Musica();
-musica1.Nome = "Formation";
-musica1.Artista = "Beyoncé";
-musica1.Duracao = 326;
-musica1.Disponivel = true;
-//musica1.EscreveDisponivel(true);
-//Console.WriteLine(musica1.LerDisponivel());
+musica1.Nome = "I'M THAT GIRL";
+musica1.Duracao = 328;
 
 Musica musica2 = new Musica();
-musica2.Nome = "Paparazzi";
-musica2.Artista = "Lady Gaga";
-musica2.Duracao = 328;
-musica2.Disponivel = false;
-//musica2.EscreveDisponivel(false);
-//Console.WriteLine(musica2.LerDisponivel());
+musica2.Nome = "COZY";
+musica2.Duracao = 330;
 
-musica1.ExibirFichaTecnica();
-musica2.ExibirFichaTecnica();
+Musica musica3 = new Musica();
+musica3.Nome = "ALIEN SUPERSTAR";
+musica3.Duracao = 339;
+
+Musica musica4 = new Musica();
+musica4.Nome = "CUFF IT";
+musica4.Duracao = 348;
+
+Musica musica5 = new Musica();
+musica5.Nome = "ENERGY (feat. BEAM)";
+musica5.Duracao = 317;
+
+Musica musica6 = new Musica();
+musica6.Nome = "BREAK MY SOUL";
+musica6.Duracao = 437;
+
+Musica musica7 = new Musica();
+musica7.Nome = "CHURCH GIRL";
+musica7.Duracao = 303;
+
+Musica musica8 = new Musica();
+musica8.Nome = "PLASTIC OFF THE SOFA";
+musica8.Duracao = 318;
+
+Musica musica9 = new Musica();
+musica9.Nome = "VIRGO'S GROOVE";
+musica9.Duracao = 307;
+
+Musica musica10 = new Musica();
+musica10.Nome = "MOVE (feat. Grace Jones & Tems)";
+musica10.Duracao = 340;
+
+Musica musica11 = new Musica();
+musica11.Nome = "HEATED";
+musica11.Duracao = 323;
+
+Musica musica12 = new Musica();
+musica12.Nome = "THIQUE";
+musica12.Duracao = 248;
+
+Musica musica13 = new Musica();
+musica13.Nome = "ALL UP IN YOUR MIND";
+musica13.Duracao = 249;
+
+Musica musica14 = new Musica();
+musica14.Nome = "AMERICA HAS A PROBLEM";
+musica14.Duracao = 341;
+
+Musica musica15 = new Musica();
+musica15.Nome = "PURE/HONEY";
+musica15.Duracao = 423;
+
+Musica musica16 = new Musica();
+musica16.Nome = "SUMMER RENAISSANCE";
+musica16.Duracao = 454;
+
+albumDaBeyonce.AdicionarMusica(musica1);
+albumDaBeyonce.AdicionarMusica(musica2);
+albumDaBeyonce.AdicionarMusica(musica3);
+albumDaBeyonce.AdicionarMusica(musica4);
+albumDaBeyonce.AdicionarMusica(musica5);
+albumDaBeyonce.AdicionarMusica(musica6);
+albumDaBeyonce.AdicionarMusica(musica7);
+albumDaBeyonce.AdicionarMusica(musica8);
+albumDaBeyonce.AdicionarMusica(musica9);
+albumDaBeyonce.AdicionarMusica(musica10);
+albumDaBeyonce.AdicionarMusica(musica11);
+albumDaBeyonce.AdicionarMusica(musica12);
+albumDaBeyonce.AdicionarMusica(musica13);
+albumDaBeyonce.AdicionarMusica(musica14);
+albumDaBeyonce.AdicionarMusica(musica15);
+albumDaBeyonce.AdicionarMusica(musica16);
+
+albumDaBeyonce.ExibirMusicasDoAlbum();
